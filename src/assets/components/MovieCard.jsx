c90748bc3e975b33900fe5fa9aba03ae410c51fe
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-07-25 21:43:47 +0545

    touch up moie card

--- src/assets/components/MovieCard.jsx
+++ src/assets/components/MovieCard.jsx
@@ -1,21 +1,34 @@
-
+import '../components/moviecard.css';
 export  const MovieCard = ({ movies })=>{
-    
+      console.log(movies)
     return (
     <>
-    <div className="movie-card">
-     <h2>MovieCard</h2>
-     <img src=""></img>
-    <p className="movie-title">MovieTitle</p>
-     <div className="movie-card--div1">
-        <span className="movie-year">2000</span>
-        <span className="duration">111</span>
-        <span className="genre">Action</span>
-     </div>
-     <p className='rating'>three stars</p>
-     <p className="description">thisis a movie</p>
+    <div className="movie-card--container">
+        <h2>Match Movies With Your Mood....</h2>
+    
+    <div className="movie-card--wrapper">
+       
+     {movies.map((movie)=>
+     
+        <div className="movie-card--elements" key={movie.id}>
+            <img src={movie.image}></img>
+           <p className="movie-title">{movie.title}</p>
+          <div className="movie-card--div1">
+           <span className="movie-year">{movie.year}</span>
+          <span className="duration">{movie.duration}</span>
+          <span className="genre">{movie['genres'].map(currGenre => <span>{currGenre}</span>)}</span>
+         </div>
+        <p className='rating'>{movie.rating}</p>
+        <p className="mood">{movie.mood}</p>
+        </div>
+  
+    )}
+     
+    
     
     </div>
+    </div>
+    
     
     </>);
 }
